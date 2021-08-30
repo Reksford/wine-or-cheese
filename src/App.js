497ec7b7c8a4ed32286801fa8cb33d8c84cc0bed
Reksford <rexfordjwoods@yahoo.com>
2021-08-30 12:39:19 -0600

    Hardcodes initial player choice

--- src/App.js
+++ src/App.js
@@ -7,17 +7,18 @@ import Button from './components/button/button';
 import Display from './components/display/display';
 
 function App() {
-  const [story, setStory] = useState({body: null, options: []})
+  const [wineOrCheese, setWineOrCheese] = useState('')
+  const [story, setStory] = useState({body: null, options: []});
   const [options, setOptions] = useState([{title:"Wine", options: null}, {title:"Cheese", options: null}]);
     // EndStory ({body: "You're Dead."});
     //options should be an empty array so no buttons thus soft locking on the end screen.
     //maybe load in footer on ending "That's all, well.... Thanks for Playing!"
 
   const storyClickHandler = (dialogSelection) => {
-    let newStory = ["storyA", "storyB"];
       if (!dialogSelection.options) {
-        //pick story a || b and setStory
-        newStory = newStory[new Date().getTime()%2]
+        //pick story a || b and setStory setOptions
+        setWineOrCheese(dialogSelection.title);
+        let newStory = dialogSelection.title
         setStory(data[newStory]);
         let nextOptions = data[newStory].options.map((el) => data[el]);
         setOptions(nextOptions);
@@ -38,7 +39,12 @@ function App() {
       <div className="Game">
         <Display>{story.body}</Display>
         <div className="Buttons">
-          {options.map((el) => <Button key={el.title} option={el} clicked={storyClickHandler}>{el.title}</Button>) }
+          {options.map((el) => <Button
+            key={el.title}
+            option={el}
+            clicked={storyClickHandler} >
+            {el.title}
+          </Button>) }
         </div>
       </div>
       {/* footer */}
